caa382e404f3edb7f7b17240c9a0685f33fccab7
Transcription Factory <transcriptionfactory@gmail.com>
2023-02-23 04:06:29 -0500

    Update pipelineER2.R
    remove EssReg:: for cleanData

--- R/pipelineER2.R
+++ R/pipelineER2.R
@@ -20,8 +20,8 @@ pipelineER2 <- function(yaml_path, steps = "all") {
   y <- as.matrix(utils::read.csv(er_input$y_path, row.names = 1)) ## not standardized
   
   ## clean step
-  x <- EssReg::cleanData(x, y)$x
-  y <- EssReg::cleanData(x, y)$y
+  x <- cleanData(x, y)$x
+  y <- cleanData(x, y)$y
   
   x_std <- scale(x, T, T)
 
