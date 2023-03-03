fcd6542f845d688db57e46f8f71e029af3d5c4ac
Transcription Factory <transcriptionfactory@gmail.com>
2023-03-03 14:38:24 -0500

    Create cleanData.R
    add cleanData

---
+++ R/cleanData.R
@@ -0,0 +1,36 @@
+#' @export
+cleanData <- function(xdata, ydata) {
+
+  # make sure in same order
+  y_correct_order <- match(rownames(xdata), rownames(ydata))
+  ydata <- ydata[y_correct_order]
+
+
+  # get median cols
+  empty_rows <- which(apply(xdata, 1, median) == 0)
+
+  # check if we need to remove any rows
+  if (length(empty_rows) > 0) {
+    # remove rows
+    xdata <- xdata[-empty_rows,]
+    ydata <- ydata[-empty_rows]
+  }
+
+  # fix ydata rownames
+  ydata <- as.matrix(ydata)
+  rownames(ydata) <- rownames(xdata)
+
+  empty_cols <- which(apply(xdata, 2, median) == 0)
+
+  # remove zero SD too
+  zero_sd <- which(apply(xdata, 2, sd) == 0)
+
+  #bad_cols <- base::union(empty_cols, zero_sd)
+  bad_cols <- zero_sd
+   # check if we need to remove any columns
+  if (length(bad_cols) > 0) {
+    xdata <- xdata[, -bad_cols]
+  }
+
+  return(list("x" = xdata, "y" = ydata))
+}
